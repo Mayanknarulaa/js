c228ef1ab62f6bc66d93ea91ed563e1b8bff0b8a
Mayank Narula <mayanknarula26@gmail.com>
2025-01-20 18:47:24 +0000

    Changes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "mayank@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
