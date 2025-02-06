d7904d176c3ae25037c1d9e8fd4a52997ca187f5
Mayank Narula <mayanknarula26@gmail.com>
2025-02-06 19:04:02 +0000

    date js

---
+++ 01_Basics/04_date.js
@@ -0,0 +1,11 @@
+// dates in JS
+
+let myDate = new Date()
+//console.log(myDate)
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
