69eed7c138988e1bda33a8e95736535a714162d8
Mayank Narula <mayanknarula26@gmail.com>
2025-02-24 18:07:06 +0000

    Minor

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -1,8 +1,12 @@
 // if
 const isUserLoggedIn = false
 
-if(isUserLoggedIn){
-    console.log(`hey!`)
-} else {
-    console.log("Get Lost")
-}
+// if(isUserLoggedIn){
+//     console.log(`hey!`)
+// } else {
+//     console.log("Get Lost")
+// }
+
+const balance = 1000
+
+if(balance > 500) console.log("Test");
