fac4ec8c99ab2ab4a0a9d991004b7c1cbcf70258
Mayank Narula <mayanknarula26@gmail.com>
2025-02-25 07:44:43 +0000

    Switch

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,41 @@
+// const month = 2
+
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("Febraury")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+
+//     default:
+//         break;
+// }
+
+// switch: string datatype
+
+const month = "Mar"
+
+switch (month) {
+    case "Jan":
+        console.log("January")
+        break;
+    case "Feb":
+        console.log("Febraury")
+        break;
+    case "Mar":
+        console.log("March")
+        break;
+    case "Apr":
+        console.log("April")
+        break;
+
+    default:
+        break;
+}
