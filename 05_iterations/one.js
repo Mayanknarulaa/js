e0db10900ac7e176218bd0f109149e30988b52d4
Mayank Narula <mayanknarula26@gmail.com>
2025-03-04 18:53:31 +0000

    for loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,16 @@
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element)
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
     
-}
+// }
 
 // console.log(element)
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is best number")
+    }
+    console.log(element);
+    
+}
