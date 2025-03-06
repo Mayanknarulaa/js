cd9493ada1ced5459df3d9a79c448436a07fe9b4
Mayank Narula <mayanknarula26@gmail.com>
2025-03-06 17:43:28 +0000

    Array with For

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -14,11 +14,18 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i < 10; i++) {
-    console.log(`Outer loop value: ${i}`)
-    for (let j = 0; j < 10; j++) {
-        console.log(`Inner Loop Value ${j} and Outer Loop Value ${i}`)
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner Loop Value ${j} and Outer Loop Value ${i}`)
         
-    }
+//     }
     
+// }
+
+let myArray = ["SuperMan", "Batman", "Spiderman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
 }
