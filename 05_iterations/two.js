0d21c1d548ce8e7459c8e7690ca45e2bc14daaf0
Mayank Narula <mayanknarula26@gmail.com>
2025-03-06 18:31:44 +0000

    while

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,5 +1,13 @@
-let index = 0
-while (index <= 10) {
-    console.log(`value of index is ${index}`);
-    index = index + 2;
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let myArray = ["Superman", "Batman", "Spiderman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
 }
