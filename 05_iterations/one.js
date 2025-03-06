888587b563402de37599702e225ab90531b1fa44
Mayank Narula <mayanknarula26@gmail.com>
2025-03-06 17:26:08 +0000

    for in for

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -6,11 +6,19 @@
 
 // console.log(element)
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-        console.log("5 is best number")
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is best number")
+//     }
+//     console.log(element);
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value: ${i}`)
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner Loop Value ${j} and Outer Loop Value ${i}`)
+        
     }
-    console.log(element);
     
 }
