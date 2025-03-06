a8bc054b275a3a800d986d1dc35cfacaaa2e1928
Mayank Narula <mayanknarula26@gmail.com>
2025-03-06 17:55:37 +0000

    Break and Continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -23,9 +23,28 @@
     
 // }
 
-let myArray = ["SuperMan", "Batman", "Spiderman"]
+// let myArray = ["SuperMan", "Batman", "Spiderman"]
 
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+// }
+
+// Break and Continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`5 is detected`);
+//         break;
+//     }
+//     console.log(index);
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`5 is detected`);
+        continue;
+    }
+    console.log(index);
+    
 }
